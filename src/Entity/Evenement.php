c7be8635fab541c374463ac322e5b490b3b3c673
sghirlouay <meriembader8@gmail.com>
2021-03-24 22:09:03 +0100

    done

---
+++ src/Entity/Evenement.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EvenementRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EvenementRepository::class)
+ */
+class Evenement
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $debut;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $fin;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $descp;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $all_day;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $id_user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDebut(): ?\DateTimeInterface
+    {
+        return $this->debut;
+    }
+
+    public function setDebut(\DateTimeInterface $debut): self
+    {
+        $this->debut = $debut;
+
+        return $this;
+    }
+
+    public function getFin(): ?\DateTimeInterface
+    {
+        return $this->fin;
+    }
+
+    public function setFin(\DateTimeInterface $fin): self
+    {
+        $this->fin = $fin;
+
+        return $this;
+    }
+
+    public function getDescp(): ?string
+    {
+        return $this->descp;
+    }
+
+    public function setDescp(string $descp): self
+    {
+        $this->descp = $descp;
+
+        return $this;
+    }
+
+    public function getAllDay(): ?bool
+    {
+        return $this->all_day;
+    }
+
+    public function setAllDay(bool $all_day): self
+    {
+        $this->all_day = $all_day;
+
+        return $this;
+    }
+
+    public function getIdUser(): ?int
+    {
+        return $this->id_user;
+    }
+
+    public function setIdUser(int $id_user): self
+    {
+        $this->id_user = $id_user;
+
+        return $this;
+    }
+}
